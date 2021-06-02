7497ab3a5a06b2a4ed853013188870462119321b
明 <84908559+Ming-1997@users.noreply.github.com>
2021-06-02 18:25:33 +0800

    Delete test.c

--- test.c
+++
@@ -1,10 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-
-#include<stdio.h>
-
-
-void my_strcpy(char* dest, char* src){	while (*src != '\0')
-	{
-		*dest = *src;		src++;		dest++;
-	}	*dest = *src;}int main(){
-	//strcpy	//¿½±´×Ö·û´®	char arr1[] = "1234567890";	char arr2[] = "bit";	my_strcpy(arr1, arr2);	printf("%s\n", arr1);	return 0;}
